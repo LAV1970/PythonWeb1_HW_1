13ec63ba1ba9d76653cd61bffcabc3ecc5ced135
lomakina0612 <lomakina0612@gmail.com>
2023-12-14 09:40:40 +0100

    +socket

--- HW_4/front-init/main.py
+++ HW_4/front-init/main.py
@@ -1,5 +1,8 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import urllib.parse
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+import json
 import os
 
 
@@ -74,5 +77,39 @@ def run(server_class=HTTPServer, handler_class=HttpHandler):
         http.server_close()
 
 
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@socketio.on("message_from_client")
+def handle_message(message):
+    username = message["username"]
+    text = message["text"]
+
+    # Здесь ты можешь добавить логику для обработки сообщения
+
+    response_message = {"username": username, "text": text}
+    socketio.emit("message_from_server", response_message)
+
+    # Сохраняем данные в JSON файл
+    data = {"username": username, "text": text}
+    save_to_json(data)
+
+
+def save_to_json(data):
+    storage_dir = os.path.join(os.getcwd(), "storage")
+    os.makedirs(storage_dir, exist_ok=True)
+
+    file_path = os.path.join(storage_dir, "data.json")
+    with open(file_path, "a") as json_file:
+        json.dump(data, json_file)
+
+
 if __name__ == "__main__":
     run()
+    socketio.run(app, debug=True)
