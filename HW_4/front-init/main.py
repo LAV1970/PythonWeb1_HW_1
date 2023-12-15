5edc6fdee35d19a5592f658859e1ad1b1c717c34
lomakina0612 <lomakina0612@gmail.com>
2023-12-15 07:45:53 +0100

    END1_1512

--- HW_4/front-init/main.py
+++ HW_4/front-init/main.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 import json
+from datetime import datetime
+import threading
 import os
-
 
 app = Flask(__name__, static_url_path="/static")
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 socketio = SocketIO(app)
+
+# Путь к папке для сохранения данных
+storage_dir = os.path.join(os.getcwd(), "storage")
+os.makedirs(storage_dir, exist_ok=True)
+
+# Путь к файлу для сохранения данных
+data_file_path = os.path.join(storage_dir, "data.json")
+
+# Загрузка данных из существующего файла
+if os.path.exists(data_file_path):
+    with open(data_file_path, "r") as json_file:
+        messages_data = json.load(json_file)
+else:
+    messages_data = {}
 
 
 @app.route("/")
@@ -24,24 +39,35 @@ def handle_message(message):
     username = message["username"]
     text = message["text"]
 
-    # Здесь ты можешь добавить логику для обработки сообщения
+    # Здесь вы можете добавить логику для обработки сообщения
 
-    response_message = {"username": username, "text": text}
+    # Создание ключа с использованием времени получения сообщения
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    key = timestamp[:-3]
+
+    # Сохранение данных в словаре
+    messages_data[key] = {"username": username, "message": text}
+
+    # Оповещение клиентов через SocketIO
+    response_message = {"username": username, "text": text, "timestamp": timestamp}
     socketio.emit("message_from_server", response_message)
 
-    # Сохраняем данные в JSON файл
-    data = {"username": username, "text": text}
-    save_to_json(data)
+    # Сохранение данных в JSON файл
+    save_to_json(messages_data)
 
 
 def save_to_json(data):
-    storage_dir = os.path.join(os.getcwd(), "storage")
-    os.makedirs(storage_dir, exist_ok=True)
-
-    file_path = os.path.join(storage_dir, "data.json")
-    with open(file_path, "w") as json_file:
+    with open(data_file_path, "w") as json_file:
         json.dump(data, json_file)
 
 
+def run_flask():
+    socketio.run(app, debug=True, use_reloader=False)
+
+
+def run_socket():
+    socketio.start_background_task(target=run_flask)
+
+
 if __name__ == "__main__":
-    socketio.run(app)
+    threading.Thread(target=run_socket).start()
